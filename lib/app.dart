3a73d5abd2b2dd55998daf86293700dc54126034
t-kurasawa <t-kurasawa@nri-digital.jp>
2023-11-04 11:19:32 +0900

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'pages/chat.dart';
+import 'pages/map.dart';
+import 'pages/qrpay.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Senbero',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
+  static const _pages = [
+    QRPayPage(),
+    ChatPage(),
+    MapPage(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'QR Pay'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+          BottomNavigationBarItem(icon: Icon(Icons.explore), label: 'Map'),
+        ],
+        type: BottomNavigationBarType.fixed,
+      )
+    );
+  }
+}
---
+++ lib/pages/qrpay.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QRPayPage extends StatelessWidget {
+  const QRPayPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR Pay'),
+      ),
+      body: const Center(
+          child: Text('QR Pay画面', style: TextStyle(fontSize: 32.0))),
+    );
+  }
+}
